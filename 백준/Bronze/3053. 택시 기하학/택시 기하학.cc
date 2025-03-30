5141a9ee3b0a58e63c25f94788a10685629d9ec7
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2025-03-30 12:34:13 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r;
+    cin >> r;
+    double pi = 3.14159265358979;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << r * r * pi << "\n";
+    cout << r * r * 2 << "\n";
+
+    return 0;
+}
