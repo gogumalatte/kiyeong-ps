b074a1c6ab837b4ccb4b9ee8a04f568ee51e0c37
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2025-06-16 19:55:08 +0900

    [Bronze IV] Title: Triangles, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int n;
+        cin >> n;
+        if(n == 0)
+        {
+            break;
+        }
+
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j <= i; ++j)
+            {
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
