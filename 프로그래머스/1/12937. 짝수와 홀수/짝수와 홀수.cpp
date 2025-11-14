c7ab85f1fd4895c61a9f2fa5597285e52cd16272
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2025-11-14 12:03:12 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if(num % 2)
+    {
+        answer = "Odd";
+    }
+    else
+    {
+        answer = "Even";
+    }
+    return answer;
+}
