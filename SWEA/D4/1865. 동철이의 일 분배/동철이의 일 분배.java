c5baf13298def0f7aca3d5fd1c806114fbf72b69
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-02 10:31:22 +0900

    [D4] Title: 동철이의 일 분배, Time: 1,954 ms, Memory: 30,964 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -4,10 +4,9 @@ import java.io.*;
 public class Solution {
 	static int N;
 	static double[][] board;
-	static int[] seq;
 	static boolean[] visited;
 	static double result;
-	static double cur;
+    
 	public static void main(String args[]) throws Exception {
 		// BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,30 +15,24 @@ public class Solution {
 		for(int test_case = 1; test_case <= T; ++test_case) {
 			N = Integer.parseInt(br.readLine());
 			board = new double[N][N];
-			seq = new int[N];
 			visited = new boolean[N];
 			result = Double.MIN_VALUE;
 			
 			for(int i = 0; i < N; ++i) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				seq[i] = i;
 				for(int j = 0; j < N; ++j) {
 					board[i][j] = Double.parseDouble(st.nextToken()) / 100;
 				}
 			}
 			
-			solve(0);
+			solve(0, 1.0);
 			System.out.printf("#%d %.6f\n", test_case, result * 100);
 		}
 	}
 	
-	private static void solve(int depth) {
-		cur = 1;
-		for(int i = 0; i < depth; ++i) {
-			cur *= board[i][seq[i]];
-		}
+	private static void solve(int depth, double cur) {
 		
-		if(cur < result) {
+		if(cur <= result) {
 			return;
 		}
 		
@@ -50,9 +43,9 @@ public class Solution {
 		
 		for(int i = 0; i < N; ++i) {
 			if(!visited[i]) {
+                if(board[depth][i] == 0) continue;
 				visited[i] = true;
-				seq[depth] = i;
-				solve(depth + 1);
+				solve(depth + 1, cur * board[depth][i]);
 				visited[i] = false;
 			}
 		}
