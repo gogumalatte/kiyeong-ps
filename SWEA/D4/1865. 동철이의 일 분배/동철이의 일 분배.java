92b8f297f08d4fbdee2700cd44c492fab983063a
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-04 15:01:56 +0900

    [D4] Title: 동철이의 일 분배, Time: 1,954 ms, Memory: 30,964 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -5,18 +5,18 @@ public class Solution {
 	static int N;
 	static double[][] board;
 	static boolean[] visited;
-	static double result;
-    
-	public static void main(String args[]) throws Exception {
+	static double res;
+	
+	public static void main(String[] args) throws Exception {
 		// BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int T = Integer.parseInt(br.readLine());
 		for(int test_case = 1; test_case <= T; ++test_case) {
 			N = Integer.parseInt(br.readLine());
 			board = new double[N][N];
 			visited = new boolean[N];
-			result = Double.MIN_VALUE;
+			res = Double.MIN_VALUE;
 			
 			for(int i = 0; i < N; ++i) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,31 +25,29 @@ public class Solution {
 				}
 			}
 			
-			solve(0, 1.0);
-			System.out.printf("#%d %.6f\n", test_case, result * 100);
+			solve(0, 1);
+			System.out.printf("#%d %.6f\n", test_case, res * 100);
 		}
 	}
 	
 	private static void solve(int depth, double cur) {
-		
-		if(cur <= result) {
+		// 종료 조건1. 현재 확률이 전역 변수로 저장된 가장 큰 확률보다 작거나 같을 때 (더 이상 커질 수 없기 때문: 확률 <= 1) 
+		if(cur <= res) {
 			return;
 		}
 		
+		// 종료 조건2. 종료 조건 1에 해당하지 않고 모든 순서가 다 정해진다면 그 값은 최대 확률임.
 		if(depth == N) {
-			result = Math.max(result, cur);
+			res = cur;
 			return;
 		}
 		
 		for(int i = 0; i < N; ++i) {
 			if(!visited[i]) {
-                if(board[depth][i] == 0) continue;
 				visited[i] = true;
 				solve(depth + 1, cur * board[depth][i]);
 				visited[i] = false;
 			}
 		}
 	}
-	
-	
 }
