f309e4748c41b1eb92d27e4c4d591d80bbaa0e23
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-09 14:55:31 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 137 ms, Memory: 29,440 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class Solution {
 	static int V, E;
-	static List<List<Integer>> list;
-	static List<Integer> res;
-	static boolean[] visited;
+	static List<List<Integer>> adj;
+	static int[] inDegree; // 진입 차수 저장
+	static StringBuilder sb;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,53 +13,50 @@ public class Solution {
 		
 		int T = 10;
 		
-		for(int tc = 1; tc <= T; ++tc) {
+		for(int tc = 1; tc <= 10; ++tc) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			list = new ArrayList<>();
-			res = new ArrayList<>();
-			visited = new boolean[V + 1];
 			
-			for(int i = 0; i <= V; ++i) {
-				list.add(new ArrayList<>());
-			}
+			adj = new ArrayList<>();
+			inDegree = new int[V + 1];
+			for(int i = 0; i <= V; ++i) adj.add(new ArrayList<>());
 			
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < E; ++i) {
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				list.get(b).add(a); // dfs 기반으로 위상정렬을 하려면 거꾸로 세워주어야 함.
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				adj.get(from).add(to);
+				inDegree[to]++;
 			}
 			
-			solve();
-			
-			System.out.print("#" + tc);
-			for(int i : res) {
-				System.out.print(" " + i);
-			}
-			System.out.println();
+			sb = new StringBuilder();
+			sb.append("#").append(tc);
+			kahnSolve();
+			System.out.println(sb.toString());
 		}
 	}
 	
-	private static void solve() {
+	private static void kahnSolve() {
+		Queue<Integer> queue = new ArrayDeque<>();
+		
 		for(int i = 1; i <= V; ++i) {
-			dfs(i);
-		}
-	}
-	
-	private static void dfs(int v) {
-		if(visited[v]) {
-			return;
-		}
-		
-		for(Integer i : list.get(v)) {
-			if(!visited[i]) {
-				dfs(i);
+			if(inDegree[i] == 0) {
+				queue.offer(i);
 			}
 		}
 		
-		res.add(v);
-		visited[v] = true;
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			sb.append(" ").append(cur);
+			
+			for(int next : adj.get(cur)) {
+				inDegree[next]--;
+				
+				if(inDegree[next] == 0) {
+					queue.offer(next);
+				}
+			}
+		}
 	}
 }
