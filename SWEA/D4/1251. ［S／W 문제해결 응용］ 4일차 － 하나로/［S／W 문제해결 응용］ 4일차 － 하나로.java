06377707ee8ad4abba17bc3fd522a2da6252ebcb
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-12 15:32:26 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 118 ms, Memory: 28,800 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,88 +1,74 @@
 import java.util.*;
 import java.io.*;
-
+ 
 public class Solution {
-	static int N;
-	static int[] x;
-	static int[] y;
-	static long[][] graph;
-	static double totalSum;
-	static int totalCnt;
-	
-	static class Edge implements Comparable<Edge> {
-		int target;
-		long weight;
-		
-		Edge(int target, long weight) {
-			this.target = target;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return Long.compare(this.weight, o.weight);
-		}
-	}
-	
-	public static void main(String[] args) throws Exception	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedReader br = new BufferedReader(new FileReader("input하나로.txt"));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; ++tc) {
-			N = Integer.parseInt(br.readLine());
-			x = new int[N];
-			y = new int[N];
-			graph = new long[N][N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; ++i) {
-				x[i] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; ++i) {
-				y[i] = Integer.parseInt(st.nextToken());
-			}
-			double E = Double.parseDouble(br.readLine());
-			for(int i = 0; i < N - 1; ++i) {
-				for(int j = i + 1; j < N; ++j) {
-					long dx = x[i] - x[j];
-					long dy = y[i] - y[j];
-					graph[i][j] = dx * dx + dy * dy;
-					graph[j][i] = graph[i][j];
-				}
-			}
-			totalSum = 0;
-			totalCnt = 0;
-			
-			prim();
-			
-			System.out.println("#" + tc + " " + Math.round(E * totalSum));
-		}
-	}
-	
-	public static void prim() {
-		boolean[] visited = new boolean[N];
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.offer(new Edge(0, 0));
-		
-		while(!pq.isEmpty()) {
-			Edge edge = pq.poll();
-			int curTarget = edge.target;
-			
-			if(visited[curTarget]) continue;
-			
-			visited[curTarget] = true;
-			totalSum += edge.weight;
-			totalCnt++;
-			
-			if(totalCnt == N) break;
-			
-			for(int next = 0; next < N; ++next) {
-				if(graph[curTarget][next] != 0 && !visited[next]) {
-					pq.offer(new Edge(next, graph[curTarget][next]));
-				}
-			}
-		}
-	}
+    static int N;
+    static double E;
+    static long[] islandsX;
+    static long[] islandsY;
+    static boolean[] islandVisited;
+    static long[] minL2s;
+    static long minL2;
+    static double minCost;
+    static long result;
+ 
+    public static long calculateL2(long x1, long y1, long x2, long y2){
+        return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+    }
+ 
+    public static void prim(){
+        minL2s[0] = 0;
+        for(int i = 1; i < N; i++){
+            minL2s[i] = Long.MAX_VALUE;
+        }
+ 
+        for(int i = 0; i < N; i++){
+            int idx = -1;
+            for(int j = 0; j < N; j++){
+                if(islandVisited[j]) continue;
+                if(idx == -1){
+                    idx = j;
+                }else{
+                    if(minL2s[idx] > minL2s[j]) idx = j;
+                }
+            }
+             
+            islandVisited[idx] = true;
+            minL2 += minL2s[idx];
+             
+            for(int j = 0; j < N; j++){
+                if(islandVisited[j]) continue;
+                minL2s[j] = Math.min(minL2s[j], calculateL2(islandsX[idx], islandsY[idx], islandsX[j], islandsY[j]));
+            }
+        }
+    }
+ 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+         
+        for(int test_case = 1; test_case <= T; test_case++){
+            N = Integer.parseInt(br.readLine().trim());
+            islandsX = new long[N];
+            islandsY = new long[N];
+            islandVisited = new boolean[N];
+            minL2s = new long[N];
+            minL2 = 0;
+ 
+            StringTokenizer st = new StringTokenizer(br.readLine().trim());
+            for(int i = 0; i < N; i++){
+                islandsX[i] = Long.parseLong(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine().trim());
+            for(int i = 0; i < N; i++){
+                islandsY[i] = Long.parseLong(st.nextToken());
+            }
+            E = Double.parseDouble(br.readLine().trim());
+ 
+            prim();
+            minCost = minL2 * E;
+            result = Math.round(minCost);
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
 }
